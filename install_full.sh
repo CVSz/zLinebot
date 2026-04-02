ca4767470b62dd1f2fc395aec07a848df9c59eaf
CVSz <seaza@msn.com>
2026-04-02 12:03:43 +0700

    Change execution command to use bash explicitly

--- install_full.sh
+++ install_full.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-exec "$ROOT_DIR/zlinebot-meta-orchestrator.sh" full-e2e
+exec "bash $ROOT_DIR/zlinebot-meta-orchestrator.sh" full-e2e
