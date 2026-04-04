487ff8037751a5f027bb153b67c3f76f637fd560
CVSz <seaza@msn.com>
2026-04-04 12:44:45 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-exec "bash $ROOT_DIR/zlinebot-meta-orchestrator.sh" docker-full
+bash "$ROOT_DIR/zlinebot-meta-orchestrator.sh" docker-full
